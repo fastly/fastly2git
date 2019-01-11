976c1a0ac6a0253ee06919cc18d29f89bf32710a
Rogier 'DocWilco' Mulhuijzen <github@bsdchicks.com>
2019-01-11 00:46:10 -0800

    Download service details as well as generated VCL
    The additional data is saved in a pretty printed JSON file, and
    allows users to figure out changes in things like logging and
    domains. Version comments are also added to the git commit message
    and the commit time is set to the value of `updated_at`.

--- lib/fastly2git.rb
+++ lib/fastly2git.rb
@@ -2,6 +2,9 @@ class Fastly2Git
   require 'fastly'
   require 'rugged'
   require 'set'
+  require 'net/http'
+  require 'json'
+  require 'time'
 
   def self.versions(clioptions)
     fastly = Fastly.new(api_key: clioptions[:apikey])
@@ -12,33 +15,80 @@ class Fastly2Git
     service.versions.find_all(&:locked)
   end
 
+  def self.details(versions, local_versions, clioptions)
+    details = {}
+
+    # Setup HTTP client because Fastly gem doesn't support parameters on service.details
+    url = URI.parse("https://api.fastly.com")
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    headers = { "Fastly-Key" => clioptions[:apikey] }
+
+    versions.each do |version|
+      vnumber = version.number
+      commitmsg = "Version #{vnumber}"
+      next if local_versions.include?(commitmsg)
+      puts "Importing version #{vnumber} details" if clioptions[:verbose]
+      resp = http.get("/service/#{version.service_id}/details?version=#{version.number}", headers)
+      if resp.kind_of?(Net::HTTPSuccess)
+        details[vnumber] = resp.body
+      else
+        puts 'Could not fetch service details' if clioptions[:verbose]
+      end
+    end
+    details
+  end
+
   def self.local_versions(repo)
     local_versions = Set.new
     unless repo.empty?
       commit = repo.last_commit
       while commit
-        local_versions.add(commit.message)
+        local_versions.add(commit.message.lines.first.strip)
         commit = commit.parents[0]
       end
     end
     local_versions
   end
 
-  def self.git(versions, repo, verbose)
-    local_versions = local_versions(repo)
+  def self.git(versions, local_versions, details, repo, verbose)
     versions.each do |version|
+      vnumber = version.number
       options = version_to_options(repo, version)
       next if local_versions.include?(options[:message])
-      puts 'Importing version...' if verbose
+      repo.index.read_tree(repo.head.target.tree) unless repo.empty?
+      puts "Importing version #{vnumber} VCL" if verbose
       begin
         vcl = version.generated_vcl
         oid = repo.write(vcl.content, :blob)
-        repo.index.read_tree(repo.head.target.tree) unless repo.empty?
         repo.index.add(path: 'generated.vcl', oid: oid, mode: 0o100644)
-        options[:tree] = repo.index.write_tree(repo)
       rescue Fastly::Error
         puts 'Could not fetch VCL' if verbose
       end
+      if details[vnumber]
+        json = JSON.parse(details[vnumber])
+        node = json["version"]
+        updated_at = node["updated_at"]
+        node.delete("updated_at");
+        node.delete("created_at");
+        node.delete("deleted_at");
+        node.delete("deployed");
+        node.delete("locked");
+        node.delete("active");
+        node.delete("testing");
+        node.delete("staging");
+        comment = node["comment"]
+        if comment and comment != ""
+          options[:message] << "\n\n#{comment}\n"
+        end
+        if updated_at
+          options[:author][:time] = Time.parse(updated_at)
+          options[:committer][:time] = Time.parse(updated_at)
+        end
+        oid = repo.write(JSON.pretty_generate(node), :blob)
+        repo.index.add(path: 'details.json', oid: oid, mode: 0o100644)
+      end
+      options[:tree] = repo.index.write_tree(repo)
       Rugged::Commit.create(repo, options)
     end
     repo.checkout_head(strategy: :safe)
@@ -46,7 +96,6 @@ class Fastly2Git
 
   def self.version_to_options(repo, version)
     options = {}
-    options[:tree] = repo.index.write_tree(repo)
     options[:author] = {
       email: 'user@fastly.com',
       name: 'Fastly User',
--- test/test_fastly2git.rb
+++ test/test_fastly2git.rb
@@ -7,6 +7,7 @@ class Fastyl2GitTest < Minitest::Test
   def version1
     v1 = Minitest::Mock.new
     v1.expect :number, 1
+    v1.expect :number, 1
     v1.expect :locked, true
     v1.expect :generated_vcl,
               (Minitest::Mock.new.expect :content, "Version 1\n")
@@ -16,6 +17,7 @@ class Fastyl2GitTest < Minitest::Test
   def version2
     v2 = Minitest::Mock.new
     v2.expect :number, 2
+    v2.expect :number, 2
     v2.expect :locked, true
     v2.expect :generated_vcl,
               (Minitest::Mock.new.expect :content, "Version 2\nVersion 2\n")
@@ -24,6 +26,7 @@ class Fastyl2GitTest < Minitest::Test
 
   def version3
     v3 = Minitest::Mock.new
+    v3.expect :number, 3
     v3.expect :number, 3
     v3.expect :locked, true
     v3.expect :generated_vcl,
@@ -37,7 +40,11 @@ def test_oneshot_single_export
 
     repo = Rugged::Repository.init_at(dir)
 
-    Fastly2Git.git([version1], repo, false)
+    local_versions = Fastly2Git.local_versions(repo)
+
+    details = { 1 => "{\"foo\":\"bar\",\"version\":{\"updated_at\":\"2018-12-25T12:34:56Z\",\"comment\":\"Some details\"}}" }
+
+    Fastly2Git.git([version1], local_versions, details, repo, false)
     repo = Rugged::Repository.new(dir)
 
     ref = repo.head
@@ -46,8 +53,12 @@ def test_oneshot_single_export
 
     commit = repo.lookup(target_id)
     assert_equal commit.type, :commit
-    assert_equal commit.message, 'Version 1'
-    tree = commit.tree.first
+    assert_equal commit.message, "Version 1\n\nSome details\n"
+    assert_equal commit.author[:time], Time.parse("2018-12-25 12:34:56 +0000")
+    tree = commit.tree[0]
+    assert_equal tree[:name], 'details.json'
+    assert_equal repo.lookup(tree[:oid]).content, "{\n  \"comment\": \"Some details\"\n}"
+    tree = commit.tree[1]
     assert_equal tree[:name], 'generated.vcl'
     assert_equal repo.lookup(tree[:oid]).content, "Version 1\n"
 
@@ -63,7 +74,13 @@ def test_oneshot_single_export
 
     repo = Rugged::Repository.init_at(dir)
 
-    Fastly2Git.git([version1, version2, version3], repo, false)
+    local_versions = Fastly2Git.local_versions(repo)
+
+    details = { 1 => "{\"foo\":\"bar\",\"version\":{\"comment\":\"Some details\"}}",
+                2 => "{\"foo\":\"baz\",\"version\":{\"comment\":\"\"}}",
+                3 => "{\"foo\":\"bax\",\"version\":{\"comment\":\"multi\\nline\"}}" }
+
+    Fastly2Git.git([version1, version2, version3], local_versions, details, repo, false)
     repo = Rugged::Repository.new(dir)
 
     ref = repo.head
@@ -72,25 +89,33 @@ def test_oneshot_single_export
 
     commit = repo.lookup(target_id)
     assert_equal commit.type, :commit
-    assert_equal commit.message, 'Version 3'
-
-    tree = commit.tree.first
+    assert_equal commit.message, "Version 3\n\nmulti\nline\n"
+    tree = commit.tree[0]
+    assert_equal tree[:name], 'details.json'
+    assert_equal repo.lookup(tree[:oid]).content, "{\n  \"comment\": \"multi\\nline\"\n}"
+    tree = commit.tree[1]
     assert_equal tree[:name], 'generated.vcl'
     assert_equal repo.lookup(tree[:oid]).content, "Version 3\nVersion 3\nVersion 3\n"
-    commit = commit.parents[0]
 
+    commit = commit.parents[0]
     assert_equal commit.type, :commit
     assert_equal commit.message, 'Version 2'
-    tree = commit.tree.first
+    tree = commit.tree[0]
+    assert_equal tree[:name], 'details.json'
+    assert_equal repo.lookup(tree[:oid]).content, "{\n  \"comment\": \"\"\n}"
+    tree = commit.tree[1]
     assert_equal tree[:name], 'generated.vcl'
     assert_equal repo.lookup(tree[:oid]).content, "Version 2\nVersion 2\n"
 
     commit = commit.parents[0]
     assert_equal commit.type, :commit
-    assert_equal commit.message, 'Version 1'
-    tree = commit.tree.first
+    assert_equal commit.message, "Version 1\n\nSome details\n"
+    tree = commit.tree[0]
+    assert_equal tree[:name], 'details.json'
+    assert_equal repo.lookup(tree[:oid]).content, "{\n  \"comment\": \"Some details\"\n}"
+    tree = commit.tree[1]
     assert_equal tree[:name], 'generated.vcl'
-    assert_equal repo.lookup(tree[:oid]).content, "Version 1\n"
+ 
 
     commit = commit.parents[0]
     assert_equal commit, nil
@@ -104,7 +129,12 @@ def test_oneshot_single_export
 
     repo = Rugged::Repository.init_at(dir)
 
-    Fastly2Git.git([version1, version2], repo, false)
+    local_versions = Fastly2Git.local_versions(repo)
+
+    details = { 1 => "{\"foo\":\"bar\",\"version\":{\"comment\":\"Some details\"}}",
+                2 => "{\"foo\":\"baz\",\"version\":{\"comment\":\"\"}}" }
+
+    Fastly2Git.git([version1, version2], local_versions, details, repo, false)
     repo = Rugged::Repository.new(dir)
 
     ref = repo.head
@@ -114,19 +144,33 @@ def test_oneshot_single_export
     commit = repo.lookup(target_id)
     assert_equal commit.type, :commit
     assert_equal commit.message, 'Version 2'
-
-    tree = commit.tree.first
+    tree = commit.tree[0]
+    assert_equal tree[:name], 'details.json'
+    assert_equal repo.lookup(tree[:oid]).content, "{\n  \"comment\": \"\"\n}"
+    tree = commit.tree[1]
     assert_equal tree[:name], 'generated.vcl'
     assert_equal repo.lookup(tree[:oid]).content, "Version 2\nVersion 2\n"
-    commit = commit.parents[0]
 
+    commit = commit.parents[0]
     assert_equal commit.type, :commit
-    assert_equal commit.message, 'Version 1'
-    tree = commit.tree.first
+    assert_equal commit.message, "Version 1\n\nSome details\n"
+    tree = commit.tree[0]
+    assert_equal tree[:name], 'details.json'
+    assert_equal repo.lookup(tree[:oid]).content, "{\n  \"comment\": \"Some details\"\n}"
+    tree = commit.tree[1]
     assert_equal tree[:name], 'generated.vcl'
     assert_equal repo.lookup(tree[:oid]).content, "Version 1\n"
 
-    Fastly2Git.git([version1, version2, version3], repo, false)
+    commit = commit.parents[0]
+    assert_equal commit, nil
+
+    local_versions = Fastly2Git.local_versions(repo)
+
+    details = { 1 => "{\"foo\":\"bar\",\"version\":{\"comment\":\"Some details\"}}",
+                2 => "{\"foo\":\"baz\",\"version\":{\"comment\":\"\"}}",
+                3 => "{\"foo\":\"bax\",\"version\":{\"comment\":\"multi\\nline\"}}" }
+
+    Fastly2Git.git([version1, version2, version3], local_versions, details, repo, false)
 
     ref = repo.head
     assert_equal ref.name, 'refs/heads/master'
@@ -134,23 +178,31 @@ def test_oneshot_single_export
 
     commit = repo.lookup(target_id)
     assert_equal commit.type, :commit
-    assert_equal commit.message, 'Version 3'
-
-    tree = commit.tree.first
+    assert_equal commit.message, "Version 3\n\nmulti\nline\n"
+    tree = commit.tree[0]
+    assert_equal tree[:name], 'details.json'
+    assert_equal repo.lookup(tree[:oid]).content, "{\n  \"comment\": \"multi\\nline\"\n}"
+    tree = commit.tree[1]
     assert_equal tree[:name], 'generated.vcl'
     assert_equal repo.lookup(tree[:oid]).content, "Version 3\nVersion 3\nVersion 3\n"
-    commit = commit.parents[0]
 
+    commit = commit.parents[0]
     assert_equal commit.type, :commit
     assert_equal commit.message, 'Version 2'
-    tree = commit.tree.first
+    tree = commit.tree[0]
+    assert_equal tree[:name], 'details.json'
+    assert_equal repo.lookup(tree[:oid]).content, "{\n  \"comment\": \"\"\n}"
+    tree = commit.tree[1]
     assert_equal tree[:name], 'generated.vcl'
     assert_equal repo.lookup(tree[:oid]).content, "Version 2\nVersion 2\n"
 
     commit = commit.parents[0]
     assert_equal commit.type, :commit
-    assert_equal commit.message, 'Version 1'
-    tree = commit.tree.first
+    assert_equal commit.message, "Version 1\n\nSome details\n"
+    tree = commit.tree[0]
+    assert_equal tree[:name], 'details.json'
+    assert_equal repo.lookup(tree[:oid]).content, "{\n  \"comment\": \"Some details\"\n}"
+    tree = commit.tree[1]
     assert_equal tree[:name], 'generated.vcl'
     assert_equal repo.lookup(tree[:oid]).content, "Version 1\n"
 
